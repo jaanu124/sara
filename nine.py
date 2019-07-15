937eca94e27938140e0a477513a2270b86f7b623
jaanu124 <52922370+jaanu124@users.noreply.github.com>
2019-07-15 07:41:26 -0700

    Update and rename nine.py to ten.py

--- nine.py
+++
@@ -1,6 +0,0 @@
-q,b=map(int,input().split())
-c=0
-t=list(map(int,input().split()))
-for i in range(b):
-   c+=t[i]
-print(c)
---
+++ ten.py
@@ -0,0 +1,6 @@
+c=int(input())
+t=0
+while c>0:
+    t+=1
+    c=c//10
+print(t)
